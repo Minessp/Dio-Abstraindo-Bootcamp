cc9e0a93265a6eaef3b3ae36939230824c55411c
Minessp <minestadionchannel@gmail.com>
2025-11-06 15:10:50 -0300

    Created Mentoria & Curso classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import org.msp.desafio.domain.Curso;
+import org.msp.desafio.domain.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso de JavaScript");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(java.time.LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
